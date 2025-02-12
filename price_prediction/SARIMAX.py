1e4855c4cd800cfb8b5c5e078c6a4674f30e556c
Msitua <91263902+AskVorting@users.noreply.github.com>
2025-02-12 10:42:46 +0100

    1

--- price_prediction/SARIMAX.py
+++ price_prediction/SARIMAX.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+from matplotlib.pylab import rcParams
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
 
-# from dataset.paths import *
+df_y = pd.read_csv(r"dataset\processed\train\train_y.csv")
 
+<<<<<<< Updated upstream
 # df = pd.read_csv(r"dataset\processed\train\train_x.csv")
 
 df = pd.read_csv("../dataset/processed/train/train_x.csv")
+=======
+result = seasonal_decompose(df_y)
+>>>>>>> Stashed changes
