3744af0b0d1b1e70d79c8ecd0a63231fc48b2d25
Voggo <90676486+Voggo@users.noreply.github.com>
2025-02-12 11:09:15 +0100

    Seasonal decomposistion

--- price_prediction/SARIMAX.py
+++ price_prediction/SARIMAX.py
@@ -8,12 +8,28 @@ from matplotlib.pylab import rcParams
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.seasonal import seasonal_decompose
 
-df_y = pd.read_csv(r"dataset\processed\train\train_y.csv")
 
-<<<<<<< Updated upstream
-# df = pd.read_csv(r"dataset\processed\train\train_x.csv")
+df = pd.read_csv("dataset/processed/train/train.csv", parse_dates=['time'], index_col='time')
 
-df = pd.read_csv("../dataset/processed/train/train_x.csv")
-=======
-result = seasonal_decompose(df_y)
->>>>>>> Stashed changes
+result = seasonal_decompose(df['price actual'], model='additative', period=12*24)
+
+trend = result.trend
+seasonal = result.seasonal
+residual = result.resid
+
+fig, axes = plt.subplots(4, 1, figsize=(10, 8), sharex=True)
+
+df['price actual'][:168*3].plot(ax=axes[0], title='Original')
+axes[0].set_ylabel('Price')
+
+trend[:168*3].plot(ax=axes[1], title='Trend')
+axes[1].set_ylabel('Trend')
+
+seasonal[:168*3].plot(ax=axes[2], title='Seasonal')
+axes[2].set_ylabel('Seasonal')
+
+residual[:168*3].plot(ax=axes[3], title='Residual')
+axes[3].set_ylabel('Residual')
+
+plt.tight_layout()
+plt.show()
